bcb9246e20ca9f922a7fdbe46f96ca6db7bf80d7
vargnaar <vargles08@gmail.com>
2022-06-07 00:20:24 +1000

    added the secret section

---
+++ secret/js/commands.js
@@ -0,0 +1,98 @@
+var youtube = "https://www.youtube.com/channel/UCQlpI3JaR-8tbAQoqEWMKIw";
+var twitter = "https://www.twitter.com/vargnaar/";
+var password = "0dayWasHere";
+var linkedin = "https://www.linkedin.com/in/vnarseth/";
+var instagram = "https://www.instagram.com/varghalladesign/";
+var github = "https://github.com/vargnaar";
+var email = 'mailto:h8mail@varghalla.com';
+
+whois = [
+  "<br>",
+  "Hey horsie, I haven't filled this in yet. I will tho. luv u.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "visitor",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube.com/varghalladesign' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/vargnaar' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/vnarseth' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/varghalladesign' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/vargnaar' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Python Digimon tamagotchi",
+  "Small javascript weather app",
+  "Varghalla Shadowboxes",
+  "Varghalla Leathercraft",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Varg?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          I really don\'t have any of these yet.',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View project ideas',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Varg Narseth (VN) Varghalla Design. All rights reserved.</span>',
+  "                                                                      ",
+  "                       .,,uod8B8bou,,.",
+  "              ..,uod8BBBBBBBBBBBBBBBBRPFT?l!i:.",
+  "         ,=m8BBBBBBBBBBBBBBBRPFT?!||||||||||||||",
+  "         !...:!TVBBBRPFT||||||||||!!^^\"\"'   ||||",
+  "         !.......:!?|||||!!^^\"\"\'            ||||",
+  "         !.........||||                     ||||",
+  "         !.........||||                     ||||",
+  "         !.........||||                     ||||",
+  "         !.........||||                     ||||",
+  "         !.........||||                     ||||",
+  "         !.........||||                     ||||",
+  "         `.........||||                    ,||||",
+  "          .;.......||||               _.-!!|||||",
+  "   .,uodWBBBBb.....||||       _.-!!|||||||||!:'",
+  "!YBBBBBBBBBBBBBBb..!|||:..-!!|||||||!iof68BBBBBb....",
+  "!..YBBBBBBBBBBBBBBb!!||||||||!iof68BBBBBBRPFT?!::   `.",
+  "!....YBBBBBBBBBBBBBBbaaitf68BBBBBBRPFT?!:::::::::     `.",
+  "!......YBBBBBBBBBBBBBBBBBBBRPFT?!::::::;:!^\"`;:::       `.",
+  "!........YBBBBBBBBBBRPFT?!::::::::::^''...::::::;         iBBbo.",
+  "`..........YBRPFT?!::::::::::::::::::::::::;iof68bo.      WBBBBbo.",
+  "  `..........:::::::::::::::::::::::;iof688888888888b.     `YBBBP^'",
+  "    `........::::::::::::::::;iof688888888888888888888b.     `",
+  "      `......:::::::::;iof688888888888888888888888888888b.",
+  "        `....:::;iof688888888888888888888888888888888899fT!",
+  "          `..::!8888888888888888888888888888888899fT|!^\"'",
+  "            `' !!988888888888888888888888899fT|!^\"'",
+  "                `!!8888888888888888899fT|!^\"'",
+  "                  `!988888888899fT|!^\"'",
+  "                    `!9899fT|!^\"'",
+  "                      `!^\"'",
+  " ",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
